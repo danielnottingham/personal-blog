268f88d67942b082696bda7c1b68c7cb4a2ee196
Daniel Nottingham <danielnottingham.ti@gmail.com>
2021-11-24 09:59:50 -0300

    install bootstrap5 and jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
+
 const { environment } = require('@rails/webpacker')
+
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery/src/jquery',
+  jQuery: 'jquery/src/jquery',
+  Rails: '@rails/ujs'
+}))
 
 module.exports = environment
